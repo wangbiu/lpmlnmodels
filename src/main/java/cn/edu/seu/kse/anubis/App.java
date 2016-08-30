9d6f1137cccffaf6051a833142f1c18bb6a2c810
王彬 <wangbiu@foxmail.com>
2016-08-30 17:27:48 +0800

    初始化项目
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

---
+++ src/main/java/cn/edu/seu/kse/anubis/App.java
@@ -0,0 +1,13 @@
+package cn.edu.seu.kse.anubis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
