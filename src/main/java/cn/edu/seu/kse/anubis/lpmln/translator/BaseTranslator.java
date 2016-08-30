a830527bc9bba5b71d8d31ebb9700b8c5dfad137
王彬 <wangbiu@foxmail.com>
2016-08-30 21:15:20 +0800

    添加基本翻译类
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

---
+++ src/main/java/cn/edu/seu/kse/anubis/lpmln/translator/BaseTranslator.java
@@ -0,0 +1,36 @@
+package cn.edu.seu.kse.anubis.lpmln.translator;
+
+import cn.edu.seu.kse.anubis.lpmln.model.Rule;
+
+import java.util.List;
+
+/**
+ * Created by 王彬 on 2016/8/30.
+ */
+public class BaseTranslator {
+    public String translate(List<Rule> rules){
+        StringBuilder sb=new StringBuilder();
+        String rulestr=null;
+        for(Rule r:rules){
+            if(r.isSoft()){
+                rulestr=translateHardRule(r);
+            }else {
+                rulestr=translateSoftRule(r);
+            }
+            sb.append(rulestr);
+        }
+        return sb.toString();
+    }
+
+    public String translateSoftRule(Rule rule){
+        StringBuilder sb=new StringBuilder();
+
+        return sb.toString();
+    }
+
+    public String translateHardRule(Rule rule){
+        StringBuilder sb=new StringBuilder();
+
+        return sb.toString();
+    }
+}
