6148a2fb055b6427715b41f384fb117c10414ae3
王彬 <wangbiu@foxmail.com>
2016-08-31 16:35:26 +0800

    添加测试数据
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

---
+++ src/test/java/cn/edu/seu/kse/anubis/lpmln/solver/TestSolver2.java
@@ -0,0 +1,16 @@
+package cn.edu.seu.kse.anubis.lpmln.solver;
+
+import org.junit.Test;
+
+/**
+ * Created by 王彬 on 2016/8/31.
+ */
+public class TestSolver2 {
+    private String rulefile="";
+
+    @Test
+    public void testClingo(){
+        Clingo4 clingo4=new Clingo4();
+        clingo4.call()
+    }
+}
