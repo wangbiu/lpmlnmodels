e1dfab26ecc5ecbadc13eeb40176d5be99fd820d
王彬 <wangbiu@foxmail.com>
2016-08-31 11:29:35 +0800

    添加PlogCRRuleTranslator翻译
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

--- src/test/java/cn/edu/seu/kse/anubis/lpmln/translator/ASPTranslatorTest.java
+++ src/test/java/cn/edu/seu/kse/anubis/lpmln/translator/ASPTranslatorTest.java
@@ -2,53 +2,67 @@ package cn.edu.seu.kse.anubis.lpmln.translator;
 
 import cn.edu.seu.kse.anubis.lpmln.model.Rule;
 import cn.edu.seu.kse.anubis.lpmln.syntax.SyntaxModule;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.theories.suppliers.TestedOn;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 /**
  * Created by 王彬 on 2016/8/30.
  */
 public class ASPTranslatorTest {
+    private String path="G:\\IdeaProjects\\lpmlnmodels\\src\\test\\resources\\friend.txt";
+    private File rulef=new File(path);
+    private int factor=0;
+    private List<Rule> rules=null;
+    private HashSet<String> herbrandUniverse=null;
+
+    @Before
+    public void parse() throws IOException {
+        SyntaxModule sm=new SyntaxModule();
+        rules= sm.parse(rulef);
+        factor=sm.getFactor();
+        herbrandUniverse=sm.getHerbrandUniverse();
+    }
 
     @Test
-    public void testTranslate() throws IOException {
-        String path="G:\\IdeaProjects\\lpmlnmodels\\src\\test\\resources\\friend.txt";
-        File rulef=new File(path);
-        SyntaxModule sm=new SyntaxModule();
-        List<Rule> rules= sm.parse(rulef);
+    public void testTranslate() {
+
         ASPTranslator translator=new ASPTranslator();
-        translator.setFactor(sm.getFactor());
-        translator.setHerbrandUniverse(sm.getHerbrandUniverse());
+        translator.setFactor(factor);
+        translator.setHerbrandUniverse(herbrandUniverse);
         String asprules=translator.translate(rules);
         System.out.println(asprules);
     }
 
     @Test
-    public void testDLVTranslate() throws IOException {
-        String path="G:\\IdeaProjects\\lpmlnmodels\\src\\test\\resources\\friend.txt";
-        File rulef=new File(path);
-        SyntaxModule sm=new SyntaxModule();
-        List<Rule> rules= sm.parse(rulef);
+    public void testDLVTranslate() {
         DLVTranslator translator=new DLVTranslator();
-        translator.setFactor(sm.getFactor());
-        translator.setHerbrandUniverse(sm.getHerbrandUniverse());
+        translator.setFactor(factor);
+        translator.setHerbrandUniverse(herbrandUniverse);
         String asprules=translator.translate(rules);
         System.out.println(asprules);
     }
 
     @Test
-    public void testPlogTranslator() throws IOException {
-        String path="G:\\IdeaProjects\\lpmlnmodels\\src\\test\\resources\\friend.txt";
-        File rulef=new File(path);
-        SyntaxModule sm=new SyntaxModule();
-        List<Rule> rules= sm.parse(rulef);
+    public void testPlogTranslator() {
         PlogTranslator translator=new PlogTranslator();
-        translator.setFactor(sm.getFactor());
-        translator.setHerbrandUniverse(sm.getHerbrandUniverse());
+        translator.setFactor(factor);
+        translator.setHerbrandUniverse(herbrandUniverse);
+        String asprules=translator.translate(rules);
+        System.out.println(asprules);
+    }
+
+    @Test
+    public void testPlogCRRuleTranslator(){
+        PlogCRRuleTranslator translator=new PlogCRRuleTranslator();
+        translator.setFactor(factor);
+        translator.setHerbrandUniverse(herbrandUniverse);
         String asprules=translator.translate(rules);
         System.out.println(asprules);
     }
