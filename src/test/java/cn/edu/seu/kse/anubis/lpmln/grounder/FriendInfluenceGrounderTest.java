01971092f7729e757077e156793d5199753ad890
王彬 <wangbiu@foxmail.com>
2017-03-26 16:42:47 +0800

    增加friend实验
    Signed-off-by: 王彬 <wangbiu@foxmail.com>

---
+++ src/test/java/cn/edu/seu/kse/anubis/lpmln/grounder/FriendInfluenceGrounderTest.java
@@ -0,0 +1,40 @@
+package cn.edu.seu.kse.anubis.lpmln.grounder;
+
+import org.junit.Test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by 王彬 on 2017/3/26.
+ */
+public class FriendInfluenceGrounderTest {
+    private FriendInfluenceGrounder grounder=new FriendInfluenceGrounder(5);
+
+    @Test
+    public void test(){
+
+        System.out.println(grounder.ground());
+    }
+
+    @Test
+    public void testGenTestCases() throws IOException {
+        File outf=null;
+        BufferedWriter bw =null;
+
+        for(int i=3;i<=40;i++){
+            outf=new File("f-"+i+".txt");
+            bw=new BufferedWriter(new FileWriter(outf));
+            grounder.setVarNumber(i);
+            bw.write(grounder.ground());
+            bw.write(System.lineSeparator());
+            bw.write("#show influence/2.");
+            bw.write(System.lineSeparator());
+            bw.close();
+        }
+
+    }
+
+}
