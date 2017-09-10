a042696725dcfff8976a6525a2358e76668806f8
许鸿翔 <xhx1693@foxmail.com>
2017-09-10 13:37:41 +0800

    添加接口
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

---
+++ src/main/java/cn/edu/seu/kse/anubis/lpmln/splitting/BotWithTop.java
@@ -0,0 +1,16 @@
+package cn.edu.seu.kse.anubis.lpmln.splitting;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by 许鸿翔 on 2017/9/10.
+ */
+public class BotWithTop {
+    public Set<String> getRealAnswerset(File botFile, Map<String,File> botToTop){
+        Set<String> realAnswerset = new HashSet<>();
+        return realAnswerset;
+    }
+}
