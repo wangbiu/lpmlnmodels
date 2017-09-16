300b2b2a1060137a4efb48b96f116bde13bb9ad7
许鸿翔 <xhx1693@foxmail.com>
2017-09-16 15:47:20 +0800

    小数支持，统一过滤强规则数量，可能引发并行中的factor问题，不过这个本来就存在
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

--- src/main/java/cn/edu/seu/kse/lpmln/solver/AdvancedBaseSolver.java
+++ src/main/java/cn/edu/seu/kse/lpmln/solver/AdvancedBaseSolver.java
@@ -1,8 +1,11 @@
 package cn.edu.seu.kse.lpmln.solver;
 
+import cn.edu.seu.kse.lpmln.app.LPMLNApp;
 import cn.edu.seu.kse.lpmln.model.WeightedAnswerSet;
 import cn.edu.seu.kse.lpmln.util.commandLine.AdvancedCommandLine;
+import cn.edu.seu.kse.lpmln.util.syntax.lpmln.LPMLNTranslationVisitor;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -21,8 +24,8 @@ public class AdvancedBaseSolver extends BaseSolver {
 
 //        System.out.println("result "+cmdres[0]);
 //        System.out.println("error: "+cmdres[1]);
-        List<WeightedAnswerSet> was=acmd.getWas();
-        weightedAs=was;
+        List<WeightedAnswerSet> originnalWas=acmd.getWas();
+        weightedAs=filtWas(originnalWas);
 
         totalSolverTime=acmd.getCputime();
         stats=genSolverStatisticsInfo();
@@ -41,6 +44,31 @@ public class AdvancedBaseSolver extends BaseSolver {
         sb.append("推理核心用时：").append(executeTime.get(0)).append(" ms").append(System.lineSeparator());
         executeProfile=sb.toString();
 
-        return was;
+        return weightedAs;
+    }
+
+    public List<WeightedAnswerSet> filtWas(List<WeightedAnswerSet> origin){
+        List<WeightedAnswerSet> ans = new ArrayList<>(origin.size());
+        //再次筛选level2上的最小值，并且将除factor以保证小数位数
+        int minLevel2 = Integer.MAX_VALUE;
+        int maxLevel2 = Integer.MIN_VALUE;
+        int aimLevel2=0;
+        int factor = LPMLNTranslationVisitor.getFactor();
+        for (WeightedAnswerSet was : origin) {
+            minLevel2 = Math.min(was.getWeights().get(1),minLevel2);
+            maxLevel2 = Math.max(was.getWeights().get(1),maxLevel2);
+        }
+        if(LPMLNApp.translation_type== LPMLNApp.TRANSLATION_TYPE.V1){
+            aimLevel2 = maxLevel2;
+        }else if(LPMLNApp.translation_type== LPMLNApp.TRANSLATION_TYPE.V2){
+            aimLevel2 = minLevel2;
+        }
+        for (WeightedAnswerSet was : origin) {
+            if(was.getWeights().get(1)==aimLevel2){
+                was.setFactor(factor);
+                ans.add(was);
+            }
+        }
+        return  ans;
     }
 }
