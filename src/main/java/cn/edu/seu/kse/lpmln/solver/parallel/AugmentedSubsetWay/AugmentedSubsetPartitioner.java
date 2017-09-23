eb9b044d735c3ca073e3154dea688bbfad098839
许鸿翔 <xhx1693@foxmail.com>
2017-09-23 16:33:58 +0800

    创建partitioner结构
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

--- src/main/java/cn/edu/seu/kse/lpmln/solver/parallel/AugmentedSubsetWay/AugmentedSubsetPartitioner.java
+++ src/main/java/cn/edu/seu/kse/lpmln/solver/parallel/AugmentedSubsetWay/AugmentedSubsetPartitioner.java
@@ -1,8 +1,32 @@
 package cn.edu.seu.kse.lpmln.solver.parallel.AugmentedSubsetWay;
 
+import cn.edu.seu.kse.lpmln.model.Rule;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by 许鸿翔 on 2017/9/23.
  */
 public class AugmentedSubsetPartitioner {
+    protected String policy = SPLIT_RANDOM;
+    public static final String SPLIT_RANDOM="RANDOM";
+    protected AugmentedSolver solver;
 
+    public AugmentedSubsetPartitioner(AugmentedSolver solver){
+        this.solver = solver;
+    }
+
+    public List<List<Rule>> partition(List<Rule> originSet){
+        switch (policy){
+            case SPLIT_RANDOM:
+            default:
+                return randomPartition(originSet);
+        }
+    }
+
+    public List<List<Rule>> randomPartition(List<Rule> originSet){
+        List<List<Rule>> subsets = new ArrayList<>();
+        return subsets;
+    }
 }
