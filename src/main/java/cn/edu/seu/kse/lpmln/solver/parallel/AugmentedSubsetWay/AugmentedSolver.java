822978853f841ff17826b91867782c2c634bf2a3
许鸿翔 <xhx1693@foxmail.com>
2017-09-24 20:15:04 +0800

    删除临时文件
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

--- src/main/java/cn/edu/seu/kse/lpmln/solver/parallel/AugmentedSubsetWay/AugmentedSolver.java
+++ src/main/java/cn/edu/seu/kse/lpmln/solver/parallel/AugmentedSubsetWay/AugmentedSolver.java
@@ -20,7 +20,7 @@ public class AugmentedSolver extends Clingo4 {
     private List<String> translatedFiles;
     private List<ExtraWeight> extraWeights;
     private List<AugmentedSubsetSolver> solvers;
-    private boolean deleteTranslatedFiles = false;
+    private boolean deleteTranslatedFiles = true;
     public AugmentedSolver(){
         setTranslatedFiles(new ArrayList<>());
         setThreadNums(Runtime.getRuntime().availableProcessors());
