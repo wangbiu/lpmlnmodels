10f2bce19bcbfbcaf57ed8118b417d94c852d96d
许鸿翔 <xhx1693@foxmail.com>
2018-01-17 11:07:06 +0800

    生成临时文件
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

--- src/main/java/cn/edu/seu/kse/lpmln/util/FileHelper.java
+++ src/main/java/cn/edu/seu/kse/lpmln/util/FileHelper.java
@@ -1,12 +1,18 @@
 package cn.edu.seu.kse.lpmln.util;
 
 import java.io.*;
+import java.util.UUID;
 
 /**
  * @author 许鸿翔
  * @date 2018/1/15
  */
 public class FileHelper {
+    public static File randomFile(){
+        //TODO:处理一下临时文件
+        return new File(UUID.randomUUID()+".tmp");
+    }
+
     public static void writeFile(File file, String out){
         try(BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file),"UTF-8"))){
             bw.write(out);
