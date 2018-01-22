1346519ad157cffc59a4b6fc90e6c2e0778302a6
许鸿翔 <xhx1693@foxmail.com>
2018-01-22 10:15:04 +0800

    给增强子集添加clone、unkonwnIdx
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

--- src/main/java/cn/edu/seu/kse/lpmln/model/AugmentedSubset.java
+++ src/main/java/cn/edu/seu/kse/lpmln/model/AugmentedSubset.java
@@ -7,14 +7,27 @@ import java.util.Set;
  * @author 许鸿翔
  * @date 2018/1/17
  */
-public class AugmentedSubset {
+public class AugmentedSubset implements Cloneable{
     private Set<Integer> satIdx;
     private Set<Integer> unsatIdx;
+    private Set<Integer> unknownIdx;
     private LpmlnProgram lpmlnProgram;
     public AugmentedSubset(LpmlnProgram lpmlnProgram){
         satIdx = new HashSet<>();
         unsatIdx = new HashSet<>();
+        unknownIdx = new HashSet<>();
+        for (int i=0;i<lpmlnProgram.getRules().size();i++){
+            unknownIdx.add(i);
+        }
         this.lpmlnProgram = lpmlnProgram;
+    }
+
+    @Override
+    public AugmentedSubset clone(){
+        AugmentedSubset cloned = new AugmentedSubset(lpmlnProgram);
+        cloned.getSatIdx().addAll(satIdx);
+        cloned.getUnsatIdx().addAll(unsatIdx);
+        return cloned;
     }
 
     public Set<Integer> getSatIdx() {
@@ -39,5 +52,13 @@ public class AugmentedSubset {
 
     public void setLpmlnProgram(LpmlnProgram lpmlnProgram) {
         this.lpmlnProgram = lpmlnProgram;
+    }
+
+    public Set<Integer> getUnknownIdx() {
+        return unknownIdx;
+    }
+
+    public void setUnknownIdx(Set<Integer> unknownIdx) {
+        this.unknownIdx = unknownIdx;
     }
 }
