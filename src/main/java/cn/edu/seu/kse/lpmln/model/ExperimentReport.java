bf73745b9ebfeb9b87899bc58674ba7d1e7f1a2c
许鸿翔 <xhx1693@foxmail.com>
2018-03-08 11:48:07 +0800

    修改report模型
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

--- src/main/java/cn/edu/seu/kse/lpmln/model/ExperimentReport.java
+++ src/main/java/cn/edu/seu/kse/lpmln/model/ExperimentReport.java
@@ -1,56 +1,21 @@
 package cn.edu.seu.kse.lpmln.model;
 
-import cn.edu.seu.kse.lpmln.solver.impl.LPMLNBaseSolver;
-
-import java.util.ArrayList;
-import java.util.List;
+import net.sf.json.JSONObject;
 
 /**
  * @author 许鸿翔
  * @date 2018/3/5
  */
 public class ExperimentReport {
-    public ExperimentReport() {
-        solvers = new ArrayList<>();
-        time = new ArrayList<>();
-        setStatus("success");
-    }
-
-    /**
-     * 实验名称
-     */
     protected String experimentName;
+    protected String solver;
+    protected String totalTime;
+    protected String solveTime;
+    protected String date;
+    protected String processors;
 
-    /**
-     * 实验时间
-     */
-    protected List<String> time;
-
-    /**
-     * 实验是否成功
-     */
-    protected String status;
-
-    /**
-     * 实验中使用的推理机
-     * TODO:可能需要修改接口
-     */
-    protected List<LPMLNBaseSolver> solvers;
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public List<LPMLNBaseSolver> getSolvers() {
-        return solvers;
-    }
-
-    public void setSolvers(List<LPMLNBaseSolver> solvers) {
-        this.solvers = solvers;
+    public String tojson(){
+        return JSONObject.fromObject(this).toString();
     }
 
     public String getExperimentName() {
@@ -61,11 +26,43 @@ public class ExperimentReport {
         this.experimentName = experimentName;
     }
 
-    public List<String> getTime() {
-        return time;
+    public String getSolver() {
+        return solver;
     }
 
-    public void setTime(List<String> time) {
-        this.time = time;
+    public void setSolver(String solver) {
+        this.solver = solver;
+    }
+
+    public String getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(String totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public String getSolveTime() {
+        return solveTime;
+    }
+
+    public void setSolveTime(String solveTime) {
+        this.solveTime = solveTime;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getProcessors() {
+        return processors;
+    }
+
+    public void setProcessors(String processors) {
+        this.processors = processors;
     }
 }
