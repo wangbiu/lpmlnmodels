3ee95772fd422f6d3df87c700ef32e56d2fa207a
许鸿翔 <xhx1693@foxmail.com>
2018-03-12 10:40:58 +0800

    重构cmdline
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

--- src/main/java/cn/edu/seu/kse/lpmln/util/commandLine/AdvancedCommandLine.java
+++ src/main/java/cn/edu/seu/kse/lpmln/util/commandLine/AdvancedCommandLine.java
@@ -1,16 +1,12 @@
 package cn.edu.seu.kse.lpmln.util.commandLine;
 
 import cn.edu.seu.kse.lpmln.model.WeightedAnswerSet;
-import cn.edu.seu.kse.lpmln.util.ErrorStreamLogger;
 import cn.edu.seu.kse.lpmln.util.LpmlnThreadPool;
-import org.apache.commons.lang3.SystemUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.LinkedBlockingDeque;
@@ -18,127 +14,22 @@ import java.util.concurrent.LinkedBlockingDeque;
 /**
  * Created by 王彬 on 2017/3/25.
  */
-public class AdvancedCommandLine {
-    final public static int WINDOWS_NT=1;
-    final public static int UNIX_LIKE=0;
+public class AdvancedCommandLine extends BaseCommandLine {
+
     protected LinkedBlockingDeque<String> progOut;
     protected int ansProcessorNums=2;
     protected List<ClingoResultProcessor> processors;
     protected List<WeightedAnswerSet> was=null;
-    protected double cputime=0;
     final public String killSig="--END of Process--";
-    final private static Logger logger= LogManager.getLogger(AdvancedCommandLine.class.getName());
-    private LpmlnThreadPool threadPool;
-
+    private static Logger logger= LogManager.getLogger(AdvancedCommandLine.class.getName());
 
     public AdvancedCommandLine(LpmlnThreadPool threadPool) {
-        this.setThreadPool(threadPool);
+        super(threadPool);
         progOut = new LinkedBlockingDeque<String>();
     }
 
-    protected int getOS(){
-        int os;
-        if(SystemUtils.IS_OS_WINDOWS){
-            os=WINDOWS_NT;
-        }else if(SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_UNIX){
-            os=UNIX_LIKE;
-        }else{
-            throw new RuntimeException("不支持的操作系统!");
-        }
-
-        return os;
-    }
-
-    protected String[] getCommandInterperter(){
-        int osType=getOS();
-        String[] cmd =new String [3];
-        if(osType==WINDOWS_NT){
-            cmd[0]="cmd.exe";
-            cmd[1]="/c";
-        }
-        else if(osType==UNIX_LIKE){
-            cmd[0]="sh";
-            cmd[1]="-c";
-        }
-        return cmd;
-    }
-
-    public void call(String command){
-        String[] cmd = getCommandInterperter();
-        cmd[2]=command;
-        BufferedReader br=null;
-        Process p= null;
-        StringBuilder res=null;
-        String as=null;
-
-        try {
-//            System.out.println(Arrays.asList(cmd));
-            p=Runtime.getRuntime().exec(cmd);
-
-            //标准错误流
-            final InputStream stderr=p.getErrorStream();
-
-            //单独的读取错误流的进程
-            threadPool.execute(new ErrorStreamLogger(stderr));
-
-            String line=null;
-            //标准输出
-            br=new BufferedReader(new InputStreamReader(p.getInputStream()));
-            boolean isBegin=true;
-            int cnt=1;
-
-            //启动回答集处理线程
-            startAnswerSetProcess();
-
-            String linespe=System.lineSeparator();
-
-            while((line=br.readLine())!=null){
-                if(isBegin){
-                    if(line.startsWith("Answer:")){
-                        isBegin=false;
-                        res=new StringBuilder();
-                    }else {
-                        continue;
-                    }
-                }
-                res.append(line);
-                res.append(linespe);
-                if(cnt == 300){
-                    if(res.indexOf("OPTIMUM FOUND")<0){
-                        progOut.add(res.toString());
-                        res=new StringBuilder();
-                        cnt=0;
-                    }
-                }
-                cnt++;
-            }
-            if(!isBegin){
-                progOut.add(res.toString());
-            }
-            int exitVal = p.waitFor();
-            //进程结束后发出终止信号
-            for(int i=0;i<ansProcessorNums;i++){
-                progOut.add(killSig);
-            }
-
-        } catch (IOException|InterruptedException ex) {
-            logger.error(ex.getMessage());
-        }
-        finally {
-            if (br!=null){
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        }
-        stopAnswerSetProcess();
-        clearStatus();
-    }
-
-    protected void startAnswerSetProcess(){
+    @Override
+    protected void startResultProcess(BufferedReader br) throws IOException, InterruptedException {
         processors=new ArrayList<>();
         ClingoResultProcessor crp;
         for(int i=0;i<ansProcessorNums;i++){
@@ -147,9 +38,45 @@ public class AdvancedCommandLine {
             threadPool.execute(crp);
         }
 
+        String line=null;
+        String linespe=System.lineSeparator();
+        boolean isBegin=true;
+        int cnt=1;
+        StringBuilder res=null;
+
+        while((line=br.readLine())!=null){
+            if(isBegin){
+                if(line.startsWith("Answer:")){
+                    isBegin=false;
+                    res=new StringBuilder();
+                }else {
+                    continue;
+                }
+            }
+            res.append(line);
+            res.append(linespe);
+            if(cnt == 300){
+                if(res.indexOf("OPTIMUM FOUND")<0){
+                    progOut.add(res.toString());
+                    res=new StringBuilder();
+                    cnt=0;
+                }
+            }
+            cnt++;
+        }
+        if(!isBegin){
+            progOut.add(res.toString());
+        }
+        int exitVal = cmdProcess.waitFor();
+        //进程结束后发出终止信号
+        for(int i=0;i<ansProcessorNums;i++){
+            progOut.add(killSig);
+        }
+
     }
 
-    protected void stopAnswerSetProcess(){
+    @Override
+    protected void stopResultProcess(){
         threadPool.waitDone();
         logger.debug("command execution done.");
         was=new ArrayList<>();
@@ -164,7 +91,9 @@ public class AdvancedCommandLine {
         }
     }
 
+    @Override
     protected void clearStatus(){
+        super.clearStatus();
         processors.clear();
         processors=null;
 
@@ -207,13 +136,5 @@ public class AdvancedCommandLine {
 
     public void setCputime(double cputime) {
         this.cputime = cputime;
-    }
-
-    public LpmlnThreadPool getThreadPool() {
-        return threadPool;
-    }
-
-    public void setThreadPool(LpmlnThreadPool threadPool) {
-        this.threadPool = threadPool;
     }
 }
