0d92d4e24df83fe876ec14d4740941efdec2f67c
许鸿翔 <xhx1693@foxmail.com>
2018-04-01 11:24:34 +0800

    生成规则限制
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

--- src/main/java/cn/edu/seu/kse/lpmln/model/HeuristicAugmentedSubset.java
+++ src/main/java/cn/edu/seu/kse/lpmln/model/HeuristicAugmentedSubset.java
@@ -1,6 +1,7 @@
 package cn.edu.seu.kse.lpmln.model;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -15,10 +16,14 @@ public class HeuristicAugmentedSubset extends AugmentedSubset {
      * 010表示原子a只支持a
      * 101表示原子可以是not a或者-a
      */
-    protected HashMap<String,Short> headRestrict;
-    protected HashMap<String,Short> bodyRestrict;
-    protected HashMap<String,Short>[] headRestrictList;
-    protected HashMap<String,Short>[] bodyRestrictList;
+    protected HashMap<String,Integer> headRestrict;
+    protected HashMap<String,Integer> bodyRestrict;
+    protected HashMap<String,Integer>[] headRestrictList;
+    protected HashMap<String,Integer>[] bodyRestrictList;
+    /**
+     * lit所有状态都可行
+     */
+    protected static final int TRUE=7;
 
     public HeuristicAugmentedSubset(LpmlnProgram lpmlnProgram) {
         super(lpmlnProgram);
@@ -29,17 +34,54 @@ public class HeuristicAugmentedSubset extends AugmentedSubset {
         List<Rule> rules = lpmlnProgram.getRules();
         for (int idx : unknownIdx) {
             Rule r = rules.get(idx);
-            HashMap<String,Short> rheadRestrict = new HashMap<>();
-            HashMap<String,Short> rbodyRestrict = new HashMap<>();
-            r.getHead().forEach(lit->restrict(lit,rheadRestrict));
-            r.getNegativeBody().forEach(lit->restrict(lit,rbodyRestrict));
-            r.getPositiveBody().forEach(lit->restrict(lit,rbodyRestrict));
+            HashMap<String,Integer> rheadRestrict = new HashMap<>();
+            HashMap<String,Integer> rbodyRestrict = new HashMap<>();
+            r.getHead().forEach(lit->restrict(lit,rheadRestrict,true));
+            r.getNegativeBody().forEach(lit->restrict(lit,rbodyRestrict,false));
+            r.getPositiveBody().forEach(lit->restrict(lit,rbodyRestrict,false));
             headRestrictList[idx] = rheadRestrict;
             bodyRestrictList[idx] = rbodyRestrict;
         }
     }
 
-    protected void restrict(String lit,HashMap<String,Short> restrict){
+    /**
+     * 生成对于文字的要求
+     * @param lit   not* -? lit,比如not -a
+     * @param restrict  当前要求的map
+     * @param type  true：头部合取，false：体部析取
+     */
+    protected void restrict(String lit,HashMap<String,Integer> restrict,boolean type){
+        String realLit = lit.trim();
+        //0:弱否定，1：强否定
+        LinkedList<Integer> neg = new LinkedList<>();
+        int status = 2;
+        //not* -? lit
+        while(realLit.startsWith("not")||realLit.startsWith("-")){
+            if(realLit.startsWith("not")){
+                neg.push(0);
+                realLit = realLit.substring(3).trim();
+            }else if(realLit.startsWith("-")){
+                neg.push(1);
+                realLit = realLit.substring(1).trim();
+            }
+        }
+        while(neg.size()>0){
+            int next = neg.pop();
+            if(next==0){
+                status = (TRUE^status);
+            }else{
+                status=1;
+            }
+        }
+        if(restrict.containsKey(realLit)){
+            if(type){
+                restrict.put(realLit,status|restrict.get(realLit));
+            }else{
+                restrict.put(realLit,status&restrict.get(realLit));
+            }
+        }else{
+            restrict.put(realLit,status);
+        }
 
     }
 
