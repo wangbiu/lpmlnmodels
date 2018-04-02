e5c08ca2f32f0e0e3b4af4307b9cabbba73ad82e
许鸿翔 <xhx1693@foxmail.com>
2018-04-02 10:08:42 +0800

    创建独立solver
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

---
+++ src/main/java/cn/edu/seu/kse/lpmln/solver/parallel/independentway/IndependentSolver.java
@@ -0,0 +1,12 @@
+package cn.edu.seu.kse.lpmln.solver.parallel.independentway;
+
+import cn.edu.seu.kse.lpmln.solver.LPMLNSolver;
+import cn.edu.seu.kse.lpmln.solver.impl.LPMLNBaseSolver;
+
+/**
+ * @author 许鸿翔
+ * @date 2018/4/2
+ */
+public class IndependentSolver extends LPMLNBaseSolver implements LPMLNSolver {
+    
+}
