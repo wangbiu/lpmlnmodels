99ff8ba421e323ba56e8f4f407ae8ba55b9f5ec6
许鸿翔 <xhx1693@foxmail.com>
2018-04-02 14:27:20 +0800

    分割子程序
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

--- src/main/java/cn/edu/seu/kse/lpmln/solver/parallel/independentway/IndependentSplitter.java
+++ src/main/java/cn/edu/seu/kse/lpmln/solver/parallel/independentway/IndependentSplitter.java
@@ -1,6 +1,7 @@
 package cn.edu.seu.kse.lpmln.solver.parallel.independentway;
 
 import cn.edu.seu.kse.lpmln.model.LpmlnProgram;
+import cn.edu.seu.kse.lpmln.model.Rule;
 import cn.edu.seu.kse.lpmln.util.LpmlnProgramHelper;
 
 import java.util.*;
@@ -10,28 +11,48 @@ import java.util.*;
  * @date 2018/4/2
  */
 public class IndependentSplitter {
-    protected LpmlnProgram program;
-    protected List<LpmlnProgram> independentPrograms;
-    public IndependentSplitter(LpmlnProgram program){
-        this.program = program;
-    }
-
     /**
      * 分割子程序
      * @param program 原程序
      * @return 子程序list
      */
-    public List<LpmlnProgram> split(LpmlnProgram program){
+    public static List<LpmlnProgram> split(LpmlnProgram program){
+        List<Rule> programRuleList = program.getRules();
         List<LpmlnProgram> ind = new ArrayList<>();
         Map<String,Set<String>> dependency = LpmlnProgramHelper.getDependency(program);
         merge(dependency);
-
-
-        independentPrograms = ind;
+        dependency.forEach((k,v)->{
+            List<Rule> subRules = new ArrayList<>();
+            programRuleList.forEach(rule -> {
+                if(contain(rule,v)){
+                    subRules.add(rule);
+                }
+            });
+            ind.add(new LpmlnProgram(subRules,program.getFactor(),program.getHerbrandUniverse(),program.getMetarule()));
+        });
         return ind;
     }
 
-    protected void merge(Map<String,Set<String>> dependency){
+    private static boolean contain(Rule r, Set<String> lits){
+        for (String lit : r.getHead()) {
+            if(lits.contains(LpmlnProgramHelper.getLiteral(lit))){
+                return true;
+            }
+        }
+        for (String lit : r.getPositiveBody()) {
+            if(lits.contains(LpmlnProgramHelper.getLiteral(lit))){
+                return true;
+            }
+        }
+        for (String lit : r.getNegativeBody()) {
+            if(lits.contains(LpmlnProgramHelper.getLiteral(lit))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected static void merge(Map<String,Set<String>> dependency){
         new HashSet<>(dependency.keySet()).forEach(lit->{
             new HashSet<>(dependency.entrySet()).forEach(ent->{
                 //把lit的set合并到ent的set中
