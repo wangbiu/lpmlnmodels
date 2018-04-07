ecd6573827befca55da084d5bd1bf10e64b2afdd
许鸿翔 <xhx1693@foxmail.com>
2018-04-07 13:54:46 +0800

    mdu权重计算
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

--- src/main/java/cn/edu/seu/kse/lpmln/model/DecisionUnit.java
+++ src/main/java/cn/edu/seu/kse/lpmln/model/DecisionUnit.java
@@ -1,5 +1,7 @@
 package cn.edu.seu.kse.lpmln.model;
 
+import cn.edu.seu.kse.lpmln.util.LpmlnProgramHelper;
+
 import java.util.List;
 import java.util.Set;
 
@@ -9,12 +11,59 @@ import java.util.Set;
  */
 public class DecisionUnit {
     private Set<String> lit;
-    private double wr;
-    private double wl;
+    private int wr;
+    private int wl;
     private List<DecisionUnit> to;
     private List<DecisionUnit> from;
+    private LpmlnProgram program;
     //TODO:完成这个模型
-    public DecisionUnit(Set<String> lit){
+    public DecisionUnit(LpmlnProgram program,Set<String> lit){
         this.lit =lit;
+        this.program = program;
+    }
+
+    private void generateWeight(){
+        wl = lit.size();
+        wr=0;
+        for (Rule rule : program.getRules()) {
+            for (String headLit : rule.getHead()) {
+                if(lit.contains(LpmlnProgramHelper.getLiteral(headLit))){
+                    wr++;
+                    break;
+                }
+            }
+        }
+    }
+
+    public int getWr() {
+        return wr;
+    }
+
+    public void setWr(int wr) {
+        this.wr = wr;
+    }
+
+    public int getWl() {
+        return wl;
+    }
+
+    public void setWl(int wl) {
+        this.wl = wl;
+    }
+
+    public List<DecisionUnit> getTo() {
+        return to;
+    }
+
+    public void setTo(List<DecisionUnit> to) {
+        this.to = to;
+    }
+
+    public List<DecisionUnit> getFrom() {
+        return from;
+    }
+
+    public void setFrom(List<DecisionUnit> from) {
+        this.from = from;
     }
 }
