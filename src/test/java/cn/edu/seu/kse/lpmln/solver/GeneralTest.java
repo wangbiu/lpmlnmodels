5040aab1e32f4419dd36571c639e381d295bf64d
许鸿翔 <xhx1693@foxmail.com>
2018-04-17 00:43:35 +0800

    整体测试
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

--- src/test/java/cn/edu/seu/kse/lpmln/solver/GeneralTest.java
+++ src/test/java/cn/edu/seu/kse/lpmln/solver/GeneralTest.java
@@ -1,37 +1,53 @@
 package cn.edu.seu.kse.lpmln.solver;
 
+import cn.edu.seu.kse.lpmln.lpmln.SolverValidator;
 import cn.edu.seu.kse.lpmln.solver.impl.LPMLNBaseSolver;
 import cn.edu.seu.kse.lpmln.solver.impl.LPMLNHybridSolver;
 import cn.edu.seu.kse.lpmln.solver.parallel.augmentedsubsetway.AugmentedSolver;
 import cn.edu.seu.kse.lpmln.solver.parallel.augmentedsubsetway.AugmentedSubsetPartitioner;
 import cn.edu.seu.kse.lpmln.solver.parallel.splittingsetway.SplittingSolver;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author 许鸿翔
  * @date 2018/4/16
  */
 public class GeneralTest {
-    public String[] testfiles = {"./src/test/resources/benchmark/bird/b-3.txt",
+    public String[] filePaths = {"./src/test/resources/benchmark/bird/b-3.txt",
             "./src/test/resources/benchmark/fire/fire_3_3.lp",
             "./src/test/resources/benchmark/fireDir/fireDir_2_2.lp",
             "./src/test/resources/benchmark/hat/hat_5.lp",
             "./src/test/resources/benchmark/maxclique/maxclique_10.lp",
             "./src/test/resources/benchmark/monty_hall/m-4.txt",
             "./src/test/resources/benchmark/bird/b-3.txt"};
-    public ArrayList<LPMLNSolver> toCheck = new ArrayList<>();
+    public List<LPMLNSolver> toCheck = new ArrayList<>();
+    public List<String> testFilePaths;
     public LPMLNSolver base = new LPMLNBaseSolver();
 
     @Before
     public void initSolvers(){
+        testFilePaths = Arrays.asList(filePaths);
         toCheck.add(asRandom());
         toCheck.add(asHeuristic());
         toCheck.add(spBot());
         toCheck.add(spLit());
         toCheck.add(hybridISA());
         toCheck.add(hybridIA());
+    }
+
+    @Test
+    public void generatTest(){
+        toCheck.forEach(lpmlnSolver -> {
+            SolverValidator solverValidator = new SolverValidator(lpmlnSolver);
+            testFilePaths.forEach(paths->{
+                solverValidator.validate(paths);
+            });
+        });
     }
 
     public LPMLNSolver asRandom(){
