9195a71a6330e6193b888e90bbb5a7629e1fe658
许鸿翔 <xhx1693@foxmail.com>
2018-05-22 22:28:04 +0800

    commoncmd
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

---
+++ src/main/java/cn/edu/seu/kse/lpmln/util/commandline/CommonCmd.java
@@ -0,0 +1,34 @@
+package cn.edu.seu.kse.lpmln.util.commandline;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * @author 许鸿翔
+ * @date 2018/5/22
+ */
+public class CommonCmd extends BaseCommandLine{
+    private StringBuilder output;
+
+    @Override
+    protected void stopResultProcess(){
+        threadPool.waitDone();
+    }
+
+    @Override
+    protected void startResultProcess(BufferedReader br) throws IOException, InterruptedException {
+        output = new StringBuilder();
+        String line;
+        while((line=br.readLine())!=null){
+            output.append(line);
+        }
+    }
+
+    public StringBuilder getOutput() {
+        return output;
+    }
+
+    public void setOutput(StringBuilder output) {
+        this.output = output;
+    }
+}
