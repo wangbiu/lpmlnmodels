e0a236b8c3a0865b3430957041c12faa6b0eeabc
许鸿翔 <xhx1693@foxmail.com>
2018-05-22 22:28:21 +0800

    grounder类创建
    Signed-off-by: 许鸿翔 <xhx1693@foxmail.com>

---
+++ src/main/java/cn/edu/seu/kse/lpmln/grounder/GringoGrounder.java
@@ -0,0 +1,12 @@
+package cn.edu.seu.kse.lpmln.grounder;
+
+/**
+ * @author 许鸿翔
+ * @date 2018/5/22
+ */
+public class GringoGrounder implements LPMLNGrounder{
+    @Override
+    public String grounding(String program) {
+        return null;
+    }
+}
---
+++ src/main/java/cn/edu/seu/kse/lpmln/grounder/LPMLNGrounder.java
@@ -0,0 +1,9 @@
+package cn.edu.seu.kse.lpmln.grounder;
+
+/**
+ * @author 许鸿翔
+ * @date 2018/5/22
+ */
+public interface LPMLNGrounder {
+    String grounding(String program);
+}
